44c92cedd2519547caffd11dc53874f7ce90ccd3
David Verduzco <david@tangentdigitalagency.com>
2024-01-30 14:03:45 -0700

    added navbar, added orgs, added sidebar, added switching, added more UI components

---
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -0,0 +1,75 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { OrganizationSwitcher } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { LayoutDashboard, Star } from "lucide-react";
+import { useSearchParams } from "next/navigation";
+
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"]
+})
+
+export const OrgSidebar = () => {
+
+  const searchParams = useSearchParams();
+  const favorites = searchParams.get('favorites');
+
+
+  return (
+    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pr-2 pt-5 bg-background">
+      <Link href='/'>
+        <div className="flex items-center gap-x-2">
+          <Image src='/logo.svg' alt="loop logo" width={35} height={35} />
+          <span className={cn('font-semibold text-2xl', font.className)}>PlanPad</span>
+        </div>
+      </Link>
+      
+      <OrganizationSwitcher
+        hidePersonal
+        appearance={{
+          elements: {
+            rootBox: {
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: '100%',
+            },
+            organizationSwitcherTrigger: {
+              padding: "6px",
+              width: "100%",
+              borderRadius: "8px",
+              border: "1px solid #E5E7EB",
+              justifyContent: "space-between",
+              backgroundColor: "white",
+            }
+         }
+
+      }}/>
+      
+      <div className="space-y-1 w-full">
+        <Button asChild variant={favorites ? "ghost" : "default"} size="lg" className="font-normal justify-start px-2 w-full">
+          <Link href='/'>
+            <LayoutDashboard className="h-4 w-4 mr-2"/>
+              Team Boards
+         
+          </Link>
+        </Button>
+
+        <Button asChild variant={favorites ? "default" : "ghost"} size="lg" className="font-normal  justify-start px-2 w-full">
+          <Link href={{pathname: '/', query: {favorites: true}}}>
+            <Star className="h-4 w-4 mr-2"/>
+              Favorite Boards
+         
+          </Link>
+        </Button>
+
+      </div>
+    </div>
+  );
+}
+ 
---
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Plus } from "lucide-react";
+import { CreateOrganization } from "@clerk/nextjs";
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
+import { Hint } from "@/components/hint";
+
+export const NewButton = () => {
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="aspect-square">
+          <Hint label="Create A New Organization" side="right" align="start" sideOffset={18} >
+        <button className="bg-primary h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition" >
+          <Plus size={24} className="text-white"/>
+            </button>
+            </Hint>
+        </div>
+        
+      </DialogTrigger>
+      <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
+        <CreateOrganization />
+      </DialogContent>
+    </Dialog>
+  )
+
+}
